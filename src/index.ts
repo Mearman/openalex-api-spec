75421762999b34961325e4793651ad30a50f8da2
Joseph Mearman <joseph@mearman.co.uk>
2024-01-05 20:16:45 +0000

    Add OpenAPI schema validation and error handling
    This commit adds code to validate the OpenAPI schema using the `openapi-schema-validator` library. If there are any validation errors, they are logged to the console and the process exits with a non-zero status code. The resulting OpenAPI specification is written to a file named `openapi.json`.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 #!/usr#!/usr/bin/env npx -y tsx
-import { merge } from "allof-merge";
 import * as fs from "fs";
+import OpenAPISchemaValidator, { OpenAPISchemaValidatorResult } from "openapi-schema-validator";
 import spec from "./spec";
 
 console.log(spec);
 
 console.log("=".repeat(80));
 
-const onMergeError = (msg: string | undefined) => {
-	throw new Error(msg);
-};
+const validator = new OpenAPISchemaValidator({
+	version: 3.1,
+});
 
-const merged = merge(spec, {onMergeError});
+const validation: OpenAPISchemaValidatorResult = validator.validate(spec);
+// console.log(validation);
+if (validation.errors.length > 0) {
+	console.error("Validation errors:");
+	// console.error(validation.errors);
+	validation.errors.forEach((err) => {
+		console.log("=".repeat(20));
+		console.error(err);
+		console.log("=".repeat(20));
+	});
+	process.exit(1);
+}
 
-console.log(merged);
-fs.writeFileSync("./merged.json", JSON.stringify(merged, null, "\t"));
+fs.writeFileSync("./openapi.json", JSON.stringify(spec, null, "\t"));
