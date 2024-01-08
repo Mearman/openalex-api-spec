498bbb4aff27e517d2dfd287b16b6fb2ce5f5fc1
Joseph Mearman <joseph@mearman.co.uk>
2024-01-08 19:42:01 +0000

    Refactor code to use refResponse for the "200" response in autocomplete endpoints.

--- src/spec/paths/autocomplete/autocomplete.ts
+++ src/spec/paths/autocomplete/autocomplete.ts
@@ -1,5 +1,5 @@
-import { refSchema } from "~/spec/components";
-import { meta } from "~/spec/components/schemas/meta";
+import { refResponse } from "~/spec/components";
+import { autocomplete200Response } from "~/spec/components/responses/autocomplete200Response";
 
 export default {
 	"/autocomplete": {
@@ -16,62 +16,7 @@ export default {
 				},
 			],
 			responses: {
-				"200": {
-					content: {
-						"application/json": {
-							schema: {
-								properties: {
-									meta: refSchema({meta}),
-									results: {
-										items: {
-											properties: {
-												cited_by_count: {
-													type: "integer",
-												},
-												display_name: {
-													type: "string",
-												},
-												entity_type: {
-													type: "string",
-												},
-												external_id: {
-													type: "string",
-												},
-												filter_key: {
-													type: "string",
-												},
-												hint: {
-													type: "string",
-												},
-												id: {
-													type: "string",
-												},
-												works_count: {
-													type: "integer",
-												},
-											},
-											required: [
-												"id",
-												"display_name",
-												"hint",
-												"cited_by_count",
-												"works_count",
-												"entity_type",
-												"external_id",
-												"filter_key",
-											],
-											type: "object",
-										},
-										type: "array",
-									},
-								},
-								required: ["meta", "results"],
-								type: "object",
-							},
-						},
-					},
-					description: "",
-				},
+				"200": refResponse({autocomplete200Response}),
 			},
 			summary: "/autocomplete",
 		},
