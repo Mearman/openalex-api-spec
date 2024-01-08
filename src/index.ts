2fadd28680eaba1d3d5616557322c7f8d9fea197
Joseph Mearman <joseph@mearman.co.uk>
2024-01-08 19:08:18 +0000

    Update index.ts with code changes
    - Import path module for file path operations
    - Replace spec import with getDoc function from "~/spec"
    - Replace dereference and getGetPaths imports with generateDereferencedSpec from "~/util"
    - Add constant DOC_PATH for the output file path
    - Update console.log statement to print doc instead of spec
    - Call validate function with doc parameter instead of spec
    - Update specOutputFilePath variable to use DOC_PATH constant
    - Write doc to the output file using fs.writeFileSync
    - Call generateDereferencedSpec function with doc parameter

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
 #!/usr#!/usr/bin/env npx -y tsx
 
 import fs, { PathOrFileDescriptor } from "fs";
-import spec from "~/spec";
-import { dereference } from "~/util/dereference";
+import path from "path";
+import { getDoc } from "~/spec";
+import { generateDereferencedSpec } from "~/util/generateDereferencedSpec";
 import { getGetPaths } from "~/util/getGetPaths";
 import { operationsWithNoRequiredParameters } from "~/util/operationsWithNoRequiredParameters";
 import { separator } from "~/util/separator";
 import { validate } from "~/util/validate";
 
+export const DOC_PATH = path.resolve(path.join(process.cwd(), "generated", "openapi.json"));
+
 async function main() {
-	console.log(JSON.stringify(spec, null, 2));
+	const doc = getDoc();
+	console.log(doc);
 	separator();
-	await validate(spec);
-	const specOutputFilePath: PathOrFileDescriptor = "./openapi.json";
-	fs.writeFileSync(specOutputFilePath, JSON.stringify(spec, null, "\t"));
-
-	const result = await dereference(spec);
-	const gets = getGetPaths(result);
-	const noRequiredParameters = operationsWithNoRequiredParameters(gets);
-	const server = new URL(spec.servers[0].url);
-	// const results =
-
-	// console.log(gets);
+	await validate(doc);
+	const specOutputFilePath: PathOrFileDescriptor = DOC_PATH;
+	fs.writeFileSync(specOutputFilePath, JSON.stringify(doc, null, "\t"));
+	const dereferencedDoc = await generateDereferencedSpec(doc);
 }
 
 (async () => {
