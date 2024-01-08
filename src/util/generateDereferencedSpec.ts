b03d8daeaf0a8ac14597c3ea56a6801e9fbd42e2
Joseph Mearman <joseph@mearman.co.uk>
2024-01-08 19:28:52 +0000

    Sort dereferenced OpenAPI spec before writing to file
    This commit adds a call to the `sortObject` function from the `util/sortObject` module in the `generateDereferencedSpec` function. The result of dereferencing the OpenAPI document is now sorted before being written to the output file.

--- src/util/generateDereferencedSpec.ts
+++ src/util/generateDereferencedSpec.ts
@@ -2,9 +2,10 @@ import fs from "fs";
 import { OpenAPI } from "openapi-types";
 import path from "path";
 import { dereference } from "~/util/dereference";
+import { sortObject } from "~/util/sortObject";
 
 export async function generateDereferencedSpec(doc: OpenAPI.Document, outputFilePath = path.join(process.cwd(), "generated", "openapi.dereferences.json")) {
-	const result = await dereference(doc);
+	const result = sortObject(await dereference(doc));
 	fs.writeFileSync(path.resolve(outputFilePath), JSON.stringify(result, null, "\t"));
 	return result;
 }
