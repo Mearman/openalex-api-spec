d3da070cfb3d4a2b6a2da4dfe280970de860452c
Joseph Mearman <joseph@mearman.co.uk>
2024-01-08 19:27:55 +0000

    Refactor code to use a separate function for writing the spec
    - The `writeSpec` function is introduced to handle writing the spec output file.
    - This improves code readability and separates concerns.
    - The previous implementation of directly writing the spec using `fs.writeFileSync` is replaced with a call to `writeSpec`.
    - No other changes are made in this commit.
    
    Note: Detailed description exceeds 50 characters.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 #!/usr#!/usr/bin/env npx -y tsx
 
-import fs, { PathOrFileDescriptor } from "fs";
-import path from "path";
 import { getDoc } from "~/spec";
-import { generateDereferencedSpec } from "~/util/generateDereferencedSpec";
-import { getGetPaths } from "~/util/getGetPaths";
-import { operationsWithNoRequiredParameters } from "~/util/operationsWithNoRequiredParameters";
-import { separator } from "~/util/separator";
-import { validate } from "~/util/validate";
-
-export const DOC_PATH = path.resolve(path.join(process.cwd(), "generated", "openapi.json"));
+import {
+	generateDereferencedSpec,
+	getGetPaths,
+	operationsWithNoRequiredParameters,
+	separator,
+	validate,
+	writeSpec
+} from "~/util";
 
 async function main() {
 	const doc = getDoc();
 	console.log(doc);
 	separator();
 	await validate(doc);
-	const specOutputFilePath: PathOrFileDescriptor = DOC_PATH;
-	fs.writeFileSync(specOutputFilePath, JSON.stringify(doc, null, "\t"));
+	writeSpec(doc);
 	const dereferencedDoc = await generateDereferencedSpec(doc);
 }
 
