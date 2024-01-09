21c9ea877cca0178c8ba2c61dc587bf6c3139f2a
Joseph Mearman <joseph@mearman.co.uk>
2024-01-09 20:13:02 +0000

    Refactor writeSpec function to use relative path for specOutputFilePath
    - Change the default value of specOutputFilePath parameter in writeSpec function to use a relative path instead of process.cwd()
    - This change allows for more flexibility when specifying the output file path
    - The new default value is "./generated/openapi.json"

--- src/util/writeSpec.ts
+++ src/util/writeSpec.ts
@@ -3,7 +3,7 @@ import { OpenAPI } from "openapi-types";
 import path from "path";
 import { sortObject } from "~/util/sortObject";
 
-export function writeSpec(doc: OpenAPI.Document, specOutputFilePath: string = path.resolve(path.join(process.cwd(), "generated", "openapi.json")), spacing: number | "\t" = "\t") {
+export function writeSpec(doc: OpenAPI.Document, specOutputFilePath: string = path.resolve(path.join("./", "generated", "openapi.json")), spacing: number | "\t" = "\t") {
 	doc = sortObject(doc);
 	fs.writeFileSync(specOutputFilePath, JSON.stringify(doc, null, spacing));
 }
