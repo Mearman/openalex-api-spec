01778447b69bdb74de86e0b3340d9c5a56a077a0
Joseph Mearman <joseph@mearman.co.uk>
2024-01-11 09:13:52 +0000

    Refactor validateOAS3_1 to use OpenAPIV3_1.Document instead of OpenAPI.Document

--- src/util/validateOAS3_1.ts
+++ src/util/validateOAS3_1.ts
@@ -1,9 +1,18 @@
 import { Schema, ValidationResult, Validator } from "@cfworker/json-schema";
-import { OpenAPI } from "openapi-types";
-// import oas31 from "oas-schemas/schemas/v3.1/schema.json";
+import { OpenAPIV3_1 } from "openapi-types";
 
-export async function validateOAS3_1(spec: OpenAPI.Document): Promise<ValidationResult> {
-	const oas31 = await (await fetch("https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json")).json() as Schema;
-	const cfValidator: Validator = new Validator(oas31 as Schema, "2020-12", true);
+export async function validateOAS3_1(
+	spec: OpenAPIV3_1.Document
+): Promise<ValidationResult> {
+	const oas31 = (await (
+		await fetch(
+			"https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"
+		)
+	).json()) as Schema;
+	const cfValidator: Validator = new Validator(
+		oas31 as Schema,
+		"2020-12",
+		true
+	);
 	return cfValidator.validate(spec);
 }
