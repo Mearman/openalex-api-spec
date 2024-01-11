45e3fc9a42d233e2d614a19787636577b7e2127c
Joseph Mearman <joseph@mearman.co.uk>
2024-01-11 08:11:10 +0000

    Ensure writeSpec creates directories

--- src/util/writeSpec.ts
+++ src/util/writeSpec.ts
@@ -5,5 +5,6 @@ import { sortObject } from "~/util/sortObject";
 
 export function writeSpec(doc: OpenAPI.Document, specOutputFilePath: string = path.resolve(path.join("./", "generated", "openapi.json")), spacing: number | "\t" = "\t") {
 	doc = sortObject(doc);
+	fs.mkdirSync(path.dirname(specOutputFilePath), { recursive: true });
 	fs.writeFileSync(specOutputFilePath, JSON.stringify(doc, null, spacing));
 }
