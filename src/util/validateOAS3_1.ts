b860122b6ac8ae9546410128165b05db7b3b71b6
Joseph Mearman <joseph@mearman.co.uk>
2024-01-11 08:10:25 +0000

    Refactor validateOAS3_1.ts to fetch OAS 3.1 schema dynamically
    - Import oas31 from "openapi-types" instead of "oas-schemas/schemas/v3.1/schema.json"
    - Fetch the OAS 3.1 schema from GitHub and assign it to oas31 variable
    - Update cfValidator with the fetched schema for validation

--- src/util/validateOAS3_1.ts
+++ src/util/validateOAS3_1.ts
@@ -1,8 +1,9 @@
 import { Schema, ValidationResult, Validator } from "@cfworker/json-schema";
-import oas31 from "oas-schemas/schemas/v3.1/schema.json";
 import { OpenAPI } from "openapi-types";
+// import oas31 from "oas-schemas/schemas/v3.1/schema.json";
 
 export async function validateOAS3_1(spec: OpenAPI.Document): Promise<ValidationResult> {
+	const oas31 = await (await fetch("https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json")).json() as Schema;
 	const cfValidator: Validator = new Validator(oas31 as Schema, "2020-12", true);
 	return cfValidator.validate(spec);
 }
