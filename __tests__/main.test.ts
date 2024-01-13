bb90ed434f2b19bce388d174e8eddcfdd714fdff
Joseph Mearman <joseph@mearman.co.uk>
2024-01-13 11:18:03 +0000

    Refactor code to improve readability and maintainability
    - Refactored the `permutations` array to use a more concise syntax
    - Updated the `params` variable to handle the case when `mailto` is not defined
    - Removed unnecessary console.log statement

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import jestOpenAPI from 'jest-openapi';
+import jestOpenAPI from "jest-openapi";
 import { execSync } from "node:child_process";
 import path from "path";
 
-const DEFAULT_DEREFERENCED_SPEC_PATH = "./generated/openapi.3.0.json"
-jestOpenAPI((path.resolve(DEFAULT_DEREFERENCED_SPEC_PATH)));
+const DEFAULT_DEREFERENCED_SPEC_PATH = "./generated/openapi.3.0.json";
+jestOpenAPI(path.resolve(DEFAULT_DEREFERENCED_SPEC_PATH));
 
 const entities = [
 	"",
@@ -19,16 +19,16 @@ const entities = [
 
 const testUrls = [
 	"https://api.openalex.org",
-	...entities.map(entity => `https://api.openalex.org/${entity}/random`),
-	...entities.map(entity => `https://api.openalex.org/${entity}`),
+	...entities.map((entity) => `https://api.openalex.org/${entity}/random`),
+	...entities.map((entity) => `https://api.openalex.org/${entity}`),
 ];
 
 function getGitEmail(): string {
-	const gitEmail = execSync('git config user.email').toString().trim();
+	const gitEmail = execSync("git config user.email").toString().trim();
 	if (gitEmail.match(/.+@.+\..+/)) {
 		return gitEmail;
 	} else {
-		return
+		return;
 	}
 }
 
@@ -37,28 +37,27 @@ function getMailto(): any {
 }
 
 describe.each(entities)(`%s API tests`, (entity) => {
-	const permutations = [
-		"random",
-		""
-	].map(suffix => [
-			"https://api.openalex.org",
-			entity,
-			entity ? suffix : ""
-		].filter(x => x)
-			.join("/")
-	).filter((value, index, self) => self.indexOf(value) === index)
-	const params = [
-		`mailto=${getMailto()}`,
-		"per_page=1",
-	].filter(x => x).join("&");
+	const permutations = ["random", ""]
+		.map((suffix) =>
+			["https://api.openalex.org", entity, entity ? suffix : ""]
+				.filter((x) => x)
+				.join("/")
+	)
+		.filter((value, index, self) => self.indexOf(value) === index);
+	let params: string[] | string = ["per_page=1"];
+	const mailto = getMailto();
+	if (mailto) {
+		params.push(`mailto=${mailto}`);
+	}
+	params = params.filter((x) => x).join("&");
 
 	test.each(permutations)(`%s should satisfy OpenAPI spec`, async (url) => {
 		for (let i = 0; i < 3; i++) {
-			console.log({url});
+			console.log({ url });
 			const res = await axios.get(`${url}?${params}`);
 			console.log({
 				// ...res,
-				data: JSON.stringify(res.data)
+				data: JSON.stringify(res.data),
 			});
 			expect(res.status).toEqual(200);
 
