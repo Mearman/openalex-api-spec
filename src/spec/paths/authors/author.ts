654143018c55f99bf35c60f78a4a0b551cade3fe
Joseph Mearman <joseph@mearman.co.uk>
2024-01-13 10:39:20 +0000

    Update authorGet200 response schema
    - Updated the authorGet200 response schema to include additional properties such as scopus and wikipedia.
    - Removed unnecessary imports and unused code.
    - Added a new endpoint to get a random author.

--- src/spec/paths/authors/author.ts
+++ src/spec/paths/authors/author.ts
@@ -1,10 +1,6 @@
-import { refParameter, refSchema } from "~/spec/components";
+import { refParameter, refResponse } from "~/spec/components";
 import { select } from "~/spec/components/parameters/select";
-import { affiliations } from "~/spec/components/schemas/affiliations";
-import { concepts } from "~/spec/components/schemas/concepts";
-import { countsByYear } from "~/spec/components/schemas/countsByYear";
-import { institutionObject } from "~/spec/components/schemas/institutionObject";
-import { institutionsArray } from "~/spec/components/schemas/institutionsArray";
+import { authorGet200 } from "~/spec/components/responses/authorGet200Response";
 
 const links: Record<string, LinkObject> = {
 	getInstitution: {
@@ -15,101 +11,42 @@ const links: Record<string, LinkObject> = {
 	},
 };
 
-export const author = {
-	"/authors/{id}": {
-		get: {
-			description: "Get a single author by id",
-			operationId: "getAuthor",
-			parameters: [
-				{
-					in: "path",
-					name: "id",
-					required: true,
-					schema: {
-						type: "string",
-					},
-				},
-				refParameter({ select }),
-			],
-			responses: {
-				"200": {
-					content: {
-						"application/json": {
-							schema: {
-								properties: {
-									affiliations: refSchema({ affiliations }),
-									cited_by_count: {
-										type: "integer",
-									},
-									counts_by_year: refSchema({ countsByYear }),
-									created_date: {
-										type: "string",
-									},
-									display_name: {
-										type: "string",
-									},
-									display_name_alternatives: {
-										items: {
-											type: "string",
-										},
-										type: "array",
-									},
-									id: {
-										type: "string",
-									},
-									ids: {
-										properties: {
-											openalex: {
-												type: "string",
-											},
-											orcid: {
-												type: "string",
-											},
-										},
-										required: ["openalex"],
-										type: "object",
-									},
-									last_known_institution: refSchema({ institutionObject }),
-									last_known_institutions: refSchema({ institutionsArray }),
-									orcid: {
-										type: "string",
-									},
-									summary_stats: {
-										properties: {
-											"2yr_mean_citedness": {
-												type: "number",
-											},
-											h_index: {
-												type: "integer",
-											},
-											i10_index: {
-												type: "integer",
-											},
-										},
-										required: ["2yr_mean_citedness", "h_index", "i10_index"],
-										type: "object",
-									},
-									updated_date: {
-										type: "string",
-									},
-									works_api_url: {
-										type: "string",
-									},
-									works_count: {
-										type: "integer",
-									},
-									x_concepts: refSchema({ concepts }),
-								},
-								required: ["id", "display_name"],
-								type: "object",
-							},
-						},
-					},
-					description: "Successful response",
-					links,
-				},
-			},
-			summary: "Get Author",
+let authorIdParam = {
+	in: "path",
+	name: "id",
+	required: true,
+	schema: {
+		pattern: "^A[0-9]+$",
+		type: "string",
+	},
+} satisfies ParameterObject;
+
+const authorById = {
+	get: {
+		description: "Get a single author by id",
+		operationId: "getAuthor",
+		parameters: [
+			refParameter({authorIdParam}),
+			refParameter({select}),
+		],
+		responses: {
+			"200": refResponse({authorGet200})
 		},
+		summary: "Get Author",
+	},
+};
+let randomAuthor = {
+	...authorById,
+	get: {
+		...authorById.get,
+		operationId: "getRandomAuthor",
+		parameters: [
+			refParameter({select}),
+		],
+		summary: "Get Random Author",
 	}
+};
+export const author = {
+	"/authors/{id}": authorById,
+	"/authors/random": randomAuthor
 } satisfies PathsObject;
