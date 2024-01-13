661487d60a3caeb3d9a22383aa9e60aeb0c5ae7b
Joseph Mearman <joseph@mearman.co.uk>
2024-01-13 11:08:31 +0000

    Add jest.config.ts file
    This commit adds a new file, jest.config.ts, which contains the Jest configuration for the project. The configuration includes transforming JavaScript files with TypeScript support, enabling bail on test failures, setting verbose output, and specifying the test environment as Node.js.

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+import { jsWithTsESM } from 'ts-jest/presets';
+// import { defaults as tsjPreset  } from 'ts-jest/presets'
+// import { defaultsESM  } from 'ts-jest/presets';
+// import { jsWithTs  } from 'ts-jest/presets';
+// import { jsWithTsESM } from 'ts-jest/presets';
+// import { jsWithBabel  } from 'ts-jest/presets';
+// import { jsWithBabelESM  } from 'ts-jest/presets';
+
+const jestConfig: JestConfigWithTsJest = {
+	transform: {
+		...jsWithTsESM.transform,
+	},
+	bail: true,
+	verbose: true,
+	testEnvironment: 'node',
+};
+
+export default jestConfig;
