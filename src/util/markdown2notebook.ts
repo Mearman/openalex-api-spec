3d251873ad4304d3ecc7c04bce2c74651dc57912
Joseph Mearman <joseph@mearman.co.uk>
2024-01-14 20:10:22 +0000

    Add functionality to remove specific cells and lines from Markdown to Jupyter Notebook conversion
    This commit adds the ability to remove specific cells and lines during the conversion process from Markdown to Jupyter Notebook. The `convertMarkdownToJupyterNotebook` function now accepts optional parameters `removeCells` and `removeLines`, which are arrays of regular expressions used for filtering out unwanted content. This allows for more flexibility in customizing the resulting Jupyter Notebook.

--- src/util/markdown2notebook.ts
+++ src/util/markdown2notebook.ts
@@ -34,32 +34,67 @@ interface JupyterNotebook {
 }
 
 // Function to convert Markdown to a Jupyter Notebook
-function convertMarkdownToJupyterNotebook(markdown: string): JupyterNotebook {
-	const segments = markdown.split(/(```(?:python|bash|sh)\n[\s\S]*?\n```)/g);
+function convertMarkdownToJupyterNotebook(
+	markdown: string,
+	removeCells?: RegExp[],
+	removeLines?: RegExp[]
+): JupyterNotebook {
+	const segments = markdown
+		.trim()
+		.split(/(```(?:python|bash|sh)\n[\s\S]*?\n```)/g);
 
-	const cells = segments.map((segment): NotebookCell => {
-		if (segment.startsWith("```python")) {
-			return {
-				cell_type: "code",
-				metadata: {},
-				source: segment.replace(/```python\n|```/g, "").split("\n"),
-			};
-		} else if (segment.startsWith("```bash") || segment.startsWith("```sh")) {
-			return {
-				cell_type: "code",
-				metadata: {},
-				source: ["%%bash"].concat(
-					segment.replace(/```(?:bash|sh)\n|```/g, "").split("\n")
-				),
-			};
-		} else {
-			return {
-				cell_type: "markdown",
-				metadata: {},
-				source: segment.split("\n"),
-			};
-		}
-	});
+	const cells = segments
+		.map((segment): NotebookCell => {
+			if (segment.startsWith("```python")) {
+				return {
+					cell_type: "code",
+					metadata: {},
+					source: segment.replace(/```python\n|```/g, "").split("\n"),
+				};
+			} else if (segment.startsWith("```bash") || segment.startsWith("```sh")) {
+				return {
+					cell_type: "code",
+					metadata: {},
+					source: ["%%bash"].concat(
+						segment.replace(/```(?:bash|sh)\n|```/g, "").split("\n")
+					),
+				};
+			} else {
+				return {
+					cell_type: "markdown",
+					metadata: {},
+					source: segment.split("\n"),
+				};
+			}
+		})
+		.filter((cell) => {
+			if (!removeCells) {
+				return true;
+			} else if (removeCells.length > 0) {
+				return !removeCells.some((regex) =>
+					cell.source.join("\n").match(regex)
+				);
+			} else {
+				return true;
+			}
+		})
+		.map((cell) => {
+			if (!(removeLines || removeLines.length > 0)) {
+				return cell;
+			}
+			cell.source = cell.source.filter((line: string) => {
+				if (removeLines.length > 0) {
+					return !removeLines.some((regex) => line.match(regex));
+				} else {
+					return true;
+				}
+			});
+			return cell;
+		})
+		.map((cell) => {
+			cell.source = cell.source.map((line) => line + "\n");
+			return cell;
+		});
 
 	const notebook: JupyterNotebook = {
 		cells: cells.filter((cell) => cell.source.join("\n").trim() !== ""),
@@ -91,7 +126,15 @@ function convertMarkdownToJupyterNotebook(markdown: string): JupyterNotebook {
 export function markdown2notebook(input: fs.PathLike, output: fs.PathLike) {
 	console.log(`Converting ${input} to ${output}`);
 	const markdownContent = fs.readFileSync(input, "utf-8");
-	const notebook = convertMarkdownToJupyterNotebook(markdownContent);
+	const notebook = convertMarkdownToJupyterNotebook(
+		markdownContent,
+		[
+			/Setuptools/gi,
+			/setup.py/gi,
+			/^\s*import mearman_openalex_api\s*$/gi
+		], [
+		/Then import the package/gi
+	]);
 	fs.writeFileSync(output, JSON.stringify(notebook, null, "\t"));
 	console.log(`Wrote ${output}`);
 	return notebook;
