665d54a3628d1ffbee9adcd788d96da703c277cc
Joseph Mearman <joseph@mearman.co.uk>
2024-01-14 06:09:47 +0000

    Add namedDocs array and test for root URL
    - Added a new array named `namedDocs` to store tuples of document names, documents, and locations.
    - Uncommented the `oasDoc31` and `oasDoc31Dereferenced` entries in the `namedDocs` array.
    - Added a new test to check the root URL using axios and expect statements.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import jestOpenAPI from "jest-openapi";
 import { execSync } from "node:child_process";
-import path from "path";
-
-const DEFAULT_DEREFERENCED_SPEC_PATH = "./generated/openapi.3.0.json";
-jestOpenAPI(path.resolve(DEFAULT_DEREFERENCED_SPEC_PATH));
+import { OpenAPI } from "openapi-types";
+import oasDoc30 from "../generated/openapi.3.0.json";
+import oasDoc30Dereferenced from "../generated/openapi.dereferenced.3.0.json";
 
 const entities = [
 	"",
@@ -15,12 +14,6 @@ const entities = [
 	"publishers",
 	"sources",
 	"works",
-];
-
-const testUrls = [
-	"https://api.openalex.org",
-	...entities.map((entity) => `https://api.openalex.org/${entity}/random`),
-	...entities.map((entity) => `https://api.openalex.org/${entity}`),
 ];
 
 function getGitEmail(): string | void {
@@ -42,31 +35,45 @@ function getMailto(): any {
 
 const TEST_RUNS: number = 3;
 
-describe.each(entities)(`%s API tests`, (entity) => {
-	const base = "https://api.openalex.org";
-	let entityUrl: string = [base, entity].join("/");
-	const listUrl = entityUrl;
+const namedDocs: [string, doc: any, string][] = [
+	["3.0 Document", oasDoc30, "generated/openapi.3.0.json"],
+	["3.0 Dereferenced", oasDoc30Dereferenced, "generated/openapi.dereferenced.3.0.json"],
+	// ["oasDoc31", oasDoc31, "generated/openapi.json"],
+	// ["oasDoc31Dereferenced", oasDoc31Dereferenced, "generated/openapi.dereferenced.json"],
+]
+describe.each(namedDocs)(`%s`, (name, doc, location) => {
+	jestOpenAPI(doc as unknown as OpenAPI.Document);
+	// jestOpenAPI(location);
+	test("Root url", async () => {
+		const res = await axios.get("https://api.openalex.org");
+		expect(res.status).toEqual(200);
+		expect(res).toSatisfyApiSpec();
+	})
 
-	const mailto = getMailto()
-	const mailtoParam = mailto ? `mailto=${mailto}` : undefined
-	const fixtures: { url: string, params: string[] }[] = [{
-		url: listUrl,
-		params: ["per_page=3", mailtoParam],
-	}, entity ? {
-		url: [entityUrl, "random"].join("/"),
-		params: [mailtoParam],
-	} : null].filter((x) => x);
+	describe.each(entities)(`%s API tests`, (entity) => {
+		const base = "https://api.openalex.org";
+		let entityUrl: string = [base, entity].join("/");
+		const listUrl = entityUrl;
 
-	describe.each(fixtures)(`%s`, (fixture) => {
-		const url = [fixture.url, fixture.params.filter((x) => x).join("&")].join("?")
-		test.each([...Array(TEST_RUNS).keys()])(`%s ${url}`, async () => {
-			console.debug(url)
-			const res = await axios.get(url);
-			expect(res.status).toEqual(200);
-			expect(res).toSatisfyApiSpec();
-			// console.debug(res.data);
-			// 	sleep for 100 ms
-			await new Promise((resolve) => setTimeout(resolve, 10));
+		const mailto = getMailto()
+		const mailtoParam = mailto ? `mailto=${mailto}` : undefined
+		const fixtures: { url: string, params: string[] }[] = [{
+			url: listUrl,
+			params: ["per_page=3", mailtoParam],
+		}, entity ? {
+			url: [entityUrl, "random"].join("/"),
+			params: [mailtoParam],
+		} : null].filter((x) => x);
+
+		describe.each(fixtures)(`%s`, (fixture) => {
+			const url = [fixture.url, fixture.params.filter((x) => x).join("&")].join("?")
+			test.each([...Array(TEST_RUNS).keys()])(`%s ${url}`, async () => {
+				console.debug(url)
+				const res = await axios.get(url);
+				expect(res.status).toEqual(200);
+				expect(res).toSatisfyApiSpec();
+				await new Promise((resolve) => setTimeout(resolve, 10));
+			});
 		});
 	});
-});
+})
