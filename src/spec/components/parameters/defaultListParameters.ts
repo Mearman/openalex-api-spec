fe3d197748402cd946ce2b75c3be107bd1a28468
Joseph Mearman <joseph@mearman.co.uk>
2024-01-15 20:01:44 +0000

    Refactor defaultListParameters.ts
    - Import api_key and cursor from ~/spec/components/parameters/api_key and ~/spec/components/parameters/cursor respectively
    - Remove group_by_underscore_param import
    - Move per_page import below page_param import

--- src/spec/components/parameters/defaultListParameters.ts
+++ src/spec/components/parameters/defaultListParameters.ts
@@ -1,6 +1,8 @@
 import { refParameter } from "~/spec/components";
+import { api_key } from "~/spec/components/parameters/api_key";
+import { cursor } from "~/spec/components/parameters/cursor";
 import { filter } from "~/spec/components/parameters/filter";
-import { group_by_param, group_by_underscore_param } from "~/spec/components/parameters/group_by_param";
+import { group_by_param } from "~/spec/components/parameters/group_by_param";
 import { page_param } from "~/spec/components/parameters/page";
 import { per_page } from "~/spec/components/parameters/per_page";
 import { sample } from "~/spec/components/parameters/sample";
@@ -9,11 +11,12 @@ import { select } from "~/spec/components/parameters/select";
 import { sort } from "~/spec/components/parameters/sort";
 
 export const defaultListParameters: (ParameterObject | ReferenceObject)[] = [
+	refParameter({ api_key }),
+	refParameter({ cursor }),
 	refParameter({ filter }),
 	refParameter({ group_by_param }),
-	refParameter({ group_by_underscore_param }),
-	refParameter({ per_page }),
 	refParameter({ page_param }),
+	refParameter({ per_page }),
 	refParameter({ sample }),
 	refParameter({ search }),
 	refParameter({ select }),
