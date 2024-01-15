5282d26466dc5d7096c8084928ebc270813a15b8
Joseph Mearman <joseph@mearman.co.uk>
2024-01-15 20:01:51 +0000

    Add new parameters to /works endpoint
    - Added group_by_param, page_param, sample, select, and sort parameters to the /works endpoint.
    - Removed filterWorks and per_page parameters from the /works endpoint.
    - Updated the response reference for 403 error.

--- src/spec/paths/works/works.ts
+++ src/spec/paths/works/works.ts
@@ -2,8 +2,13 @@ import { refParameter, refResponse } from "~/spec/components";
 import { api_key } from "~/spec/components/parameters/api_key";
 import { cursor } from "~/spec/components/parameters/cursor";
 import { filterWorks } from "~/spec/components/parameters/filterWorks";
+import { group_by_param } from "~/spec/components/parameters/group_by_param";
+import { page_param } from "~/spec/components/parameters/page";
 import { per_page } from "~/spec/components/parameters/per_page";
+import { sample } from "~/spec/components/parameters/sample";
 import { search } from "~/spec/components/parameters/search";
+import { select } from "~/spec/components/parameters/select";
+import { sort } from "~/spec/components/parameters/sort";
 import { resp403 } from "~/spec/components/responses/resp403";
 import { works } from "~/spec/components/responses/works/works";
 import { tags } from "~/spec/paths/works/tags";
@@ -14,15 +19,20 @@ export default {
 			description: "",
 			operationId: "getWorks",
 			parameters: [
-				refParameter({ filterWorks }),
 				refParameter({ api_key }),
-				refParameter({ per_page }),
 				refParameter({ cursor }),
-				refParameter({ search })
+				refParameter({ filterWorks }),
+				refParameter({ group_by_param }),
+				refParameter({ page_param }),
+				refParameter({ per_page }),
+				refParameter({ sample }),
+				refParameter({ search }),
+				refParameter({ select }),
+				refParameter({ sort }),
 			],
 			responses: {
 				"200": refResponse({ works }),
-				"403": refResponse({ resp4xx: resp403 }),
+				"403": refResponse({ resp403 }),
 				// "4xx": refResponse({resp4xx})
 			},
 			summary: "/works",
