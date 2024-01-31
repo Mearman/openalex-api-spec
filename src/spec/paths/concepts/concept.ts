3fd19c9c91ed628ac950ab0b300c01e1119bfd63
Joseph Mearman <joseph@mearman.co.uk>
2024-01-31 06:41:31 +0000

    Refactor concept API paths and add new endpoint for random concept

--- src/spec/paths/concepts/concept.ts
+++ src/spec/paths/concepts/concept.ts
@@ -3,24 +3,40 @@ import { conceptIdPatternRef } from "~/spec/components/parameters/conceptIdPatte
 import { select } from "~/spec/components/parameters/select";
 import { conceptGet200Response } from "~/spec/components/responses/concepts/conceptGet200Response";
 
-const description: string = "Get a single concept";
-export const concept = {
-	"/concepts/{id}": {
-		get: {
-			description,
-			externalDocs: {
-				description,
-				url: "https://docs.openalex.org/api-entities/concepts/get-a-single-concept",
-			},
-			operationId: "getConcept",
-			parameters: [
-				conceptIdPatternRef,
-				refParameter({ select }),
-			],
-			responses: {
-				"200": refResponse({ conceptGet200Response }),
-			},
-			summary: "/concepts/{id}"
+const getConceptById: PathItemObject = {
+	get: {
+		description: "Get a single concept",
+		externalDocs: {
+			description: "Get a single concept",
+			url: "https://docs.openalex.org/api-entities/concepts/get-a-single-concept",
 		},
+		operationId: "getConcept",
+		parameters: [
+			conceptIdPatternRef,
+			refParameter({select}),
+		],
+		responses: {
+			"200": refResponse({conceptGet200Response}),
+		},
+		summary: "/concepts/{id}"
 	},
+};
+
+const getRandomConcept: PathItemObject = {
+	get: {
+		description: "Get a random concept",
+		operationId: "getRandomConcept",
+		parameters: [
+			refParameter({select}),
+		],
+		responses: {
+			"200": refResponse({conceptGet200Response}),
+		},
+		summary: "/concepts/random"
+	},
+};
+
+export const concept = {
+	"/concepts/{id}": getConceptById,
+	"/concepts/random": getRandomConcept,
 } satisfies PathsObject;
