b40db0b594326d92e5af5d7ad85440cb0cff91dc
Joseph Mearman <joseph@mearman.co.uk>
2024-04-27 14:21:37 +0100

    Update OpenAlex TypeScript client imports and add new tests

--- __tests__/clients/typescript/index.test.ts
+++ __tests__/clients/typescript/index.test.ts
@@ -1,5 +1,7 @@
-import { createConfiguration, InfoApi } from "@mearman/openalex-typescript/dist/index";
+// import { createConfiguration, InfoApi } from "@mearman/openalex-typescript";
+// import { createConfiguration, InfoApi } from "@mearman/openalex-typescript/dist";
 
+import { InfoApi, createConfiguration } from "../../../generated/clients/typescript";
 
 describe("InfoApi", () => {
 	let configuration = createConfiguration();
@@ -9,3 +11,38 @@ describe("InfoApi", () => {
 		expect(response).toBeDefined();
 	})
 })
+
+// describe("List Endpoints", () => {
+// 	test.each([
+// 		["AuthorsApi", "getAuthors"],
+// 		["ConceptsApi", "getConcepts"],
+// 		["FundersApi", "getFunders"],
+// 		["InstitutionsApi", "getInstitutions"],
+// 		["PublishersApi", "getPublishers"],
+// 		["SourcesApi", "getSources"],
+// 		["WorksApi", "getWorks"],
+// 	])("%s.%s", async (apiName, methodName) => {
+// 		const response = await new OpenAlexApi[apiName]()[methodName]();
+// 		const body = response.body;
+// 		expect(body).toBeDefined();
+// 		expect(body.results.length).toBeGreaterThan(0);
+// 		await wait();
+// 	});
+// })
+//
+// describe("Single Endpoints", () => {
+// 	test.each([
+// 		["AuthorsApi", "getRandomAuthor"],
+// 		["ConceptsApi", "getRandomConcept"],
+// 		["FundersApi", "getRandomFunder"],
+// 		["InstitutionsApi", "getRandomInstitution"],
+// 		["PublishersApi", "getRandomPublisher"],
+// 		["SourcesApi", "getRandomSource"],
+// 		["WorksApi", "getRandomWork"],
+// 	])("%s.%s", async (apiName, methodName) => {
+// 		const response = await new OpenAlexApi[apiName]()[methodName]();
+// 		const body = response.body;
+// 		expect(body).toBeDefined();
+// 		await wait();
+// 	});
+// })
